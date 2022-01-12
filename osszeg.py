daff3c09b571075b2a5135a615f588f2fc534a4d
Csaba Gyuris <gycsaba@vasvari.hu>
2022-01-12 08:47:13 +0100

    A kész feladat

---
+++ osszeg.py
@@ -0,0 +1,26 @@
+# osszeg-gyak.py
+
+# 1. feladat
+# Határozza meg a számok összegét
+def feladat01_osszeg() -> int:
+    szamok = [1.2, 2.3, 3.2, 4.1, 2.4, 2.3, 1.9, 0.1]
+    return 0
+
+# 2. feladat
+# Határozza meg a pozitív számok összegét
+def feladat02_osszeg() -> int:
+    szamok = [1.2, -2.3, 3.2, -4.1, -2.4, 2.3, 1.9, 0.1]
+    return 0
+
+# 3. feladat
+# Határozza meg a páros számok összegét
+def feladat03_osszeg() -> int:
+    szamok = [2, 3, 4, 9, 1, 8, 6, 1]
+    return 0
+
+# 4. feladat
+# Képezze úgy az összeget, hogy a két listából az adott pozicióból mindig a nagyobb számot vegye!
+def feladat04_osszeg() -> int:
+    szamok1 = [2, 3, 4, 9, 1, 8, 6, 1]
+    szamok2 = [3, 1, 4, 1, 5, 4, 7, 0]
+    return 0
